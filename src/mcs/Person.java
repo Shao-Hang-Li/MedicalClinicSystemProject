af983016458ac9c4ebfd1bd7664cdccb38166c2b
SDXtreme <oshawanick@yahoo.ca>
2024-11-15 09:24:53 -0500

    Update Person.java
    Added constructor and default constructor
    Made Person.java into an abstract class.

--- src/mcs/Person.java
+++ src/mcs/Person.java
@@ -1,4 +1,22 @@
 package mcs;
 
-public class Person {
+public abstract class Person {
+    private String name;
+    private String dateOfBirth;
+    private String phoneNumber;
+    private String address;
+
+    public Person(String name, String dateOfBirth, String phoneNumber, String address) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public Person() {
+        this.name = "John Doe";
+        this.dateOfBirth = "24/03/1996";
+        this.phoneNumber = "514-224-8232";
+        this.address = "123 Playground Street";    
+    }
 }
