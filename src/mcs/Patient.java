51a39740ca2932d0ac673e4acb5e927cf7b51f6b
SDXtreme <oshawanick@yahoo.ca>
2024-11-16 13:10:09 -0500

    small things
    Added person info, removed random main file, changed date of birth format

--- src/mcs/Patient.java
+++ src/mcs/Patient.java
@@ -1,4 +1,14 @@
 package mcs;
 
-public class Patient {
+public class Patient extends Person{
+
+    String insuranceCompany;
+    String medicalHistory;
+
+    public Patient(String name, String dateOfBirth, String phoneNumber, String address, String insuranceCompany, String medicalHistory) {
+        super(name, dateOfBirth, phoneNumber, address);
+        this.insuranceCompany = insuranceCompany;
+        this.medicalHistory = medicalHistory;
+    }
+
 }
