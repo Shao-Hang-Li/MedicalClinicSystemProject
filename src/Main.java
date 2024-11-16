ae8df96790730dcf70b7c8ff8853bfa9410f3e8f
Shao Hang Li <shao.hang.li@outlook.com>
2024-11-16 16:09:50 -0500

    changes
    g

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,5 @@ import mcs.Person;
 
 public class Main {
     public static void main(String[] args) {
-        //testing
-        Person persons = new Person(
-
-        System.out.println(persons);
     }
 }
