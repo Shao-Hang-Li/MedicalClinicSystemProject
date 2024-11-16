929a54e1ea4e2a64bae9fd6486f101911cd50d8c
Shao Hang Li <shao.hang.li@outlook.com>
2024-11-16 13:17:01 -0500

    Mutators and accessors + Constructors

--- src/mcs/Patient.java
+++ src/mcs/Patient.java
@@ -12,19 +12,6 @@ public class Patient extends Person {
         // super class Person
         super(name, dateOfBirth, phoneNumber, address);
 
-public class Patient extends Person{
-
-    String insuranceCompany;
-    String medicalHistory;
-
-    public Patient(String name, String dateOfBirth, String phoneNumber, String address, String insuranceCompany, String medicalHistory) {
-        super(name, dateOfBirth, phoneNumber, address);
-
-        this.insuranceCompany = insuranceCompany;
-        this.medicalHistory = medicalHistory;
-    }
-
-
     // Mutators and accessors
     public String getInsuranceCompany() {
         return insuranceCompany;
