042c6b6605d701520622b3c937e5157f2dcb3c13
Shao Hang Li <shao.hang.li@outlook.com>
2024-11-16 13:10:43 -0500

    Mutators and accessors + Constructors

--- src/mcs/Doctor.java
+++ src/mcs/Doctor.java
@@ -1,4 +1,36 @@
 package mcs;
 
-public class Doctor {
+public class Doctor extends Person {
+    // Data members (Fields)
+    private String specialty;
+    private int yearsOfExperience;
+
+    // Constructor with parameters
+    public Doctor(String name, Date dateOfBirth, String phoneNumber, String address, String specialty,
+                  int yearsOfExperience) {
+        // Super class Person
+        super(name, dateOfBirth, phoneNumber, address);
+        this.specialty = specialty;
+        // Validation for yearsOfExperience (can't be negative)
+        if (yearsOfExperience < 0) {
+            throw new IllegalArgumentException("Years of experience must be a positive value");
+        }
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    // Mutators and accessors
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    // Return String representation of the Doctor object
+    @Override
 }
--- src/mcs/Patient.java
+++ src/mcs/Patient.java
@@ -1,4 +1,38 @@
 package mcs;
 
-public class Patient {
+public class Patient extends Person {
+
+    // Data members (fields)
+    private String insuranceCompany;
+    private String medicalHistory;
+
+    // Constructor with parameters
+    public Patient (String name, Date dateOfBirth, String phoneNumber, String address, String insuranceCompany,
+                    String medicalHistory) {
+        // super class Person
+        super(name, dateOfBirth, phoneNumber, address);
+
+        this.insuranceCompany = insuranceCompany;
+        this.medicalHistory = medicalHistory;
+    }
+
+    // Mutators and accessors
+    public String getInsuranceCompany() {
+        return insuranceCompany;
+    }
+
+    public void setInsuranceCompany(String insuranceCompany) {
+        this.insuranceCompany = insuranceCompany;
+    }
+
+    public String getMedicalHistory() {
+        return medicalHistory;
+    }
+
+    public void setMedicalHistory(String medicalHistory) {
+        this.medicalHistory = medicalHistory;
+    }
+
+    // Return String representation of the Patient object
+    @Override
 }
