bcb2cbad9e9b63bb3ac2d300fcb0a60fab538694
FYFYBai <qihe2000@foxmail.com>
2024-11-17 00:37:10 -0500

    Update MedicalClinicSystem.java

--- src/mcs/MedicalClinicSystem.java
+++ src/mcs/MedicalClinicSystem.java
@@ -1,11 +1,13 @@
 package mcs;
 
 import java.util.Scanner;
-import java.util.Scanner;
 // Modify as necessary
 // References: MenuOptionTester.java, Java_II_HOLs_10
 public class MedicalClinicSystem {
     static Scanner console = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        
+    }
 
 }
